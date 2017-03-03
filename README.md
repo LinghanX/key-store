28e77dcb83c37d1c5867930d7c6eee1f2fcc2065
LinghanX <linghan@me.com>
2017-03-03 15:58:07 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-## Hello
+## a distributed key-value store system
 
+This is an attempt to implement a distributed key-value store system, with a goal of maintain high availability and reliability.
---
+++ client.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if(strcmp(argv[1], "get")){
+
+    } else if (strcmp(argv[1], "set")){
+
+    }
+
+    return 0;
+}
