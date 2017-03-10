c76ccd1a13cb1273de7557f3ce84a49a2ccf9d96
LinghanX <linghan@me.com>
2017-03-09 22:20:39 -0500

    sample search

---
+++ hsearch_sample.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <search.h>
+
+static char *data[] = {
+    "alpha", "bravo","charlie", "delta", "echo",
+    "foxtrot", "golf", "hotel", "india", "juliet",
+    "kilo", "lima", "mike", "november", "oscar", "papa",
+    "quebec", "romeo", "sierra", "tango", "uniform",
+    "victor", "whisky", "x-ray", "yankee", "zulu"
+};
+
+int main(void){
+    ENTRY e, *ep;
+    int i;
+    hcreate(30);
+
+    for(i = 0; i < 24; i++){
+	e.key = data[i];
+	e.data = (void *)i;
+	ep = hsearch(e, ENTER);
+	if(ep == NULL){
+	    fprintf(stderr, "entry failed\n");
+	    exit(EXIT_FAILURE);
+	}
+    }
+
+    for(i = 22; i < 26; i++){
+	e.key = data[i];
+	ep = hsearch(e, FIND);
+	printf("%9.9s->%9.9s:%d\n", e.key, ep? ep->key: "NULL", ep ? (int)(ep->data): 0);
+    }
+
+    hdestroy();
+    exit(EXIT_SUCCESS);
+}
