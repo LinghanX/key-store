06b3ddd6b30b029f9f2f27116d8de50097716dba
junyw <wangjunyi13@gmail.com>
2018-07-08 23:18:50 -0400

    WIP

--- libnode/node.cpp
+++ libnode/node.cpp
@@ -15,3 +15,74 @@ bool Node::baz(bool foo) {
 void Node::print() {
     cout << "The node is " << nodeID.String() << endl;
 }
+
+
+class Contact {    
+public:
+    Contact* next;
+    Contact* prev;
+    Key nodeID;
+
+    Contact(Key k): nodeID(k) {
+
+    }
+    ~Contact() {
+
+    }
+};
+class ContactList
+{
+public:
+    int size;
+    Contact* head;
+
+    ContactList();
+    ~ContactList();
+    void add(Key k);
+    void print();
+};
+
+ContactList::ContactList(){
+    this->size = 0;
+    this->head = NULL;
+}
+
+ContactList::~ContactList(){
+    std::cout << "LIST DELETED";
+}
+
+void ContactList::add(Key k){
+    // Contact* node = new Contact(k);
+    // node->data = data;
+    // node->next = this->head;
+    // this->head = node;
+    // this->size++;
+}
+
+void ContactList::print(){
+    // Contact* head = this->head;
+    // int i = 1;
+    // while(head){
+    //     std::cout << i << ": " << head->data << std::endl;
+    //     head = head->next;
+    //     i++;
+    // }
+}
+
+
+
+
+class Bucket {
+private:
+    int capacity;
+    Bucket* l;
+    Bucket* r;
+    ContactList* c;
+public:
+    Bucket(int K) {
+        capacity = K;
+    }
+    ~Bucket() {
+
+    }
+};
